29102cd4cdbc0bfdd143b2b28a92066317a59833
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2026-01-29 20:17:12 +0100

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -8,8 +8,10 @@ export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=PATH_OR_URL_TO_ICON
-export DESKTOP=PATH_OR_URL_TO_DESKTOP_ENTRY
+export ICON=/usr/share/icons/hicolor/scalable/apps/com.odnoyko.valot.svg
+export DESKTOP=/usr/share/applications/com.odnoyko.valot.desktop
+export STARTUPWMCLASS=com.odnoyko.valot # Default to Wayland's wmclass. For X11, GTK_CLASS_FIX will force the wmclass to be the Wayland one.
+export GTK_CLASS_FIX=1
 
 # Deploy dependencies
 quick-sharun /usr/bin/valot
